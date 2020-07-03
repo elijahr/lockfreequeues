d4d2f84d2f5f6ab96b17167f66391a339de118c6
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-07-02 21:53:47 -0500

    Fix import

--- src/lockfreequeues.nim
+++ src/lockfreequeues.nim
@@ -1,5 +1,13 @@
-## A single-producer, single-consumer, lock-free, wait-free queue.
-## Borrows from boost's spsc_queue
+# lockfreequeues
+# © Copyright 2020 Elijah Shaw-Rutschman
+#
+# See the file "LICENSE", included in this distribution for details about the
+# copyright.
 
-import ./lockfreequeues/ringqueue
-import ./lockfreequeues/staticringqueue
+## A single-producer, single-consumer, lock-free, wait-free queue.
+##
+## Based on the algorithm outlined by Juho Snellman at
+## https://www.snellman.net/blog/archive/2016-12-13-ring-queues/
+
+import ./lockfreequeues/spscqueueshared
+import ./lockfreequeues/spscqueuestatic
