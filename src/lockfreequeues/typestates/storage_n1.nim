7ca2cf103e800ed4d31bab955067e62ec18e529a
elijahr <elijahr@users.noreply.github.com>
2025-12-04 15:17:28 -0600

    feat(typestates): add N+1-slot storage for SPSC

---
+++ src/lockfreequeues/typestates/storage_n1.nim
@@ -0,0 +1,28 @@
+# lockfreequeues # © Copyright 2020 Elijah Shaw-Rutschman # # See the file "LICENSE", included in this distribution for details about the # copyright.# lockfreequeues
+# © Copyright 2020 Elijah Shaw-Rutschman
+#
+# See the file "LICENSE", included in this distribution for details about the
+# copyright.
+
+## N+1-slot storage for SPSC queue.
+
+import ./virtual_values_n1
+
+type
+  StorageN1*[N: static int, T] = object
+    ## Storage with N+1 slots (for SPSC full/empty detection).
+    data*: array[N + 1, T]
+
+
+proc init*[N: static int, T](s: var StorageN1[N, T]) =
+  for i in 0..N:
+    s.data[i].reset()
+
+proc `[]`*[N: static int, T](s: StorageN1[N, T], idx: PhysicalSlotN1[N]): T {.inline.} =
+  s.data[idx.slotValue]
+
+proc `[]`*[N: static int, T](s: var StorageN1[N, T], idx: PhysicalSlotN1[N]): var T {.inline.} =
+  s.data[idx.slotValue]
+
+proc `[]=`*[N: static int, T](s: var StorageN1[N, T], idx: PhysicalSlotN1[N], val: T) {.inline.} =
+  s.data[idx.slotValue] = val
