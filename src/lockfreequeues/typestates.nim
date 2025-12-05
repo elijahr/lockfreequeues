3ab4c7f658a807d567f641789940089b6f53ae32
elijahr <elijahr@users.noreply.github.com>
2025-12-04 21:04:10 -0600

    feat(typestates): export spmc_pop module

--- src/lockfreequeues/typestates.nim
+++ src/lockfreequeues/typestates.nim
@@ -1,4 +1,4 @@
-# lockfreequeues # © Copyright 2020 Elijah Shaw-Rutschman # # See the file "LICENSE", included in this distribution for details about the # copyright.# lockfreequeues # © Copyright 2020 Elijah Shaw-Rutschman # # See the file "LICENSE", included in this distribution for details about the # copyright.# lockfreequeues # © Copyright 2020 Elijah Shaw-Rutschman # # See the file "LICENSE", included in this distribution for details about the # copyright.# lockfreequeues
+# lockfreequeues # © Copyright 2020 Elijah Shaw-Rutschman # # See the file "LICENSE", included in this distribution for details about the # copyright.# lockfreequeues # © Copyright 2020 Elijah Shaw-Rutschman # # See the file "LICENSE", included in this distribution for details about the # copyright.# lockfreequeues # © Copyright 2020 Elijah Shaw-Rutschman # # See the file "LICENSE", included in this distribution for details about the # copyright.# lockfreequeues # © Copyright 2020 Elijah Shaw-Rutschman # # See the file "LICENSE", included in this distribution for details about the # copyright.# lockfreequeues # © Copyright 2020 Elijah Shaw-Rutschman # # See the file "LICENSE", included in this distribution for details about the # copyright.# lockfreequeues
 # © Copyright 2020 Elijah Shaw-Rutschman
 #
 # See the file "LICENSE", included in this distribution for details about the
@@ -20,6 +20,9 @@ import ./typestates/cas
 import ./typestates/atomic_loaders
 import ./typestates/fullness_checks
 
+# Operation lifecycle typestates
+import ./typestates/spmc_pop
+
 # Old types for backward compatibility (will be removed after full migration)
 import ./typestates/fullness
 import ./typestates/atomic_values
@@ -33,6 +36,7 @@ export storage_n1
 export cas
 export atomic_loaders
 export fullness_checks
+export spmc_pop
 export fullness  # Backward compatibility
 export atomic_values  # Backward compatibility
 export index_types  # Backward compatibility
